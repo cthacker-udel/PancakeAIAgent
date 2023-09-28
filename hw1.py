32ab9573bccf5e344bc33f8cf21511d8fe2b746d
cthacker-udel <cthacker@udel.edu>
2023-09-27 22:35:30 -0400

    feat: Finished + tested BFS on test cases

--- hw1.py
+++ hw1.py
@@ -23,9 +23,9 @@ from unittest import TestCase
 #############################
 test_cases = [
     ["1w2b3b4w-b", "1w2b|3b4w\n2w|1b3b4w\n2b1b|3b4w\n1w2w3b|4w\n3w|2b1b4w\n3b2b1b|4w\n1w2w3w4w"],
+    ["1w2b3w4b-b", "1w2b|3w4b\n2w|1b3w4b\n2b1b|3w4b\n1w2w3w4b|\n4w|3b2b1b\n4b3b2b1b|\n1w2w3w4w"],
     ["1b2b3w4b-a", "1b2b|3w4b g:0, h:0\n2w|1w3w4b g:2, h:2\n2b1w3w4b| g:3, h:2\n4w|3b1b2w g:7, h:4\n4b3b1b2w| g:8, h:4\n2b1w|3w4w g:12, h:2\n1b|2w3w4w g:14, h:0\n1w2w3w4w g:15, h:0"],
     ["1w2b3w4b-a", "1w2b|3w4b g:0, h:0\n2w|1b3w4b g:2, h:2\n2b1b3w4b| g:3, h:2\n4w|3b1w2w g:7, h:4\n4b3b1w2w| g:8, h:4\n4b3b1w2w| g:8, h:4\n1w2w3w4w g:14, h:0"],
-    ["1w2b3w4b-b", "1w2b|3w4b\n2w|1b3w4b\n2b1b|3w4b\n1w2w3w4b|\n4w|3b2b1b\n4b3b2b1b|\n1w2w3w4w"],
     ["1b2b3w4b-a", "1b2b|3w4b g:0, h:0\n2w|1w3w4b g:2, h:2\n2b1w3w4b| g:3, h:2\n4w|3b1b2w g:7, h:4\n4b3b1b2w| g:8, h:4\n2b1w|3w4w g:12, h:2\n1b|2w3w4w g:14, h:0\n1w2w3w4w g:15, h:0"],
     ["1b2w3b4b-a", "1b2w3b4b| g:0, h:0\n4w|3w2b1w g:4, h:4\n4b3w|2b1w g:5, h:4\n3b|4w2b1w g:7, h:4\n3w4w|2b1w g:8, h:4\n4b3b2b1w| g:10, h:4\n1b|2w3w4w g:14, h:0\n1w2w3w4w g:15, h:0"]
 ]
@@ -137,6 +137,7 @@ class PancakeState:
         cloned = PancakeState([x.clone() for x in self.pancakes])
         cloned.num_pancakes = len(self.pancakes)
         cloned.explored = False
+        cloned.flipped = self.flipped
         return cloned
 
     def explore(self: PancakeState) -> None:
@@ -329,7 +330,7 @@ class PancakeFlippingSolver:
             return steps
         return None
 
-    def stringify_steps(self: PancakeFlippingSolver, state: PancakeState | None) -> List[str]:
+    def stringify_steps(self: PancakeFlippingSolver, state: PancakeState | None) -> str:
         """
         Stringifies all the steps made in the algorithmic process
 
@@ -338,13 +339,25 @@ class PancakeFlippingSolver:
             state (PancakeState | None): The state we are stringifying it's steps, by traversing up the parents (the steps it made before it)
 
         Returns:
-            List[str]: The list of steps executed in the process
+            List[str]: The steps separated by a \\n character
         """
-        steps = []
+        if state is None:
+            return ''
+
+        steps: List[PancakeState] = []
         while state is not None:
-            steps.append(str(state))
+            steps.append(state.clone())
             state = state.parent
-        return steps
+
+        cloned_steps = [x.clone() for x in steps]
+        for ind, each_cloned_step in enumerate(cloned_steps):
+            if '|' in str(each_cloned_step):
+                cloned_steps[ind] = each_cloned_step.flip(
+                    each_cloned_step.flipped)
+
+        stringified_cloned_steps = [
+            str(x) for x in cloned_steps[:-1][::-1]] + [str(cloned_steps[0].flip(cloned_steps[0].flipped)).replace('|', '')]
+        return '\n'.join(stringified_cloned_steps)
 
 
 def main(run_input: bool = False):
@@ -367,6 +380,8 @@ def main(run_input: bool = False):
             last_step = solver.run_algorithm()
             steps = solver.stringify_steps(last_step)
             tester.assertEqual(steps, each_test_case[1])
+            print(
+                f'--------- {each_test_case[0]} PASSED ---------\n')
 
 
 if __name__ == '__main__':
